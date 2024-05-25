c68697a07dc664cffdc0916fa2f850121b01cb67
akhwan16 <faridakhwan57@gmail.com>
2024-05-25 20:27:04 +0700

    tes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'blue-950': '#002469',
+      }
+    },
+  },
+  plugins: [],
+}
+
