5171d74d2c5f0b750fecdc10d36b4ed08f763854
akhwan16 <faridakhwan57@gmail.com>
2024-06-19 06:25:55 +0700

    sddds

--- ADMIN/rekapdivisi.php
+++ ADMIN/rekapdivisi.php
@@ -161,7 +161,7 @@
             if ($current_divisi_id !== null) {
                 echo "<div class='divisi program-divisi divisi-{$current_program_id}'>";
                 echo "<h3>{$current_divisi_nama}</h3>";
-                echo "<p>Jumlah Post: " . count($divisi_posts) . "</p>";
+                echo "<p>Jumlah Jobdesk: " . count($divisi_posts) . "</p>";
                 echo "<div class='post-box'>";
                 foreach ($divisi_posts as $post) {
                     $class = '';
@@ -192,7 +192,7 @@
             if ($current_divisi_id !== null) {
                 echo "<div class='divisi program-divisi divisi-{$current_program_id}'>";
                 echo "<h3>{$current_divisi_nama}</h3>";
-                echo "<p>Jumlah Post: " . count($divisi_posts) . "</p>";
+                echo "<p>Jumlah Jobdesk: " . count($divisi_posts) . "</p>";
                 echo "<div class='post-box'>";
                 foreach ($divisi_posts as $post) {
                     $class = '';
@@ -227,7 +227,7 @@
     if ($current_divisi_id !== null) {
         echo "<div class='divisi program-divisi divisi-{$current_program_id}'>";
         echo "<h3>{$current_divisi_nama}</h3>";
-        echo "<p>Jumlah Post: " . count($divisi_posts) . "</p>";
+        echo "<p>Jumlah Jobdesk: " . count($divisi_posts) . "</p>";
         echo "<div class='post-box'>";
         foreach ($divisi_posts as $post) {
             $class = '';
